6dfce2fcfe259ffa53fe15a7bd003fb19d8f9c50
taznimt <taznimanwar06@gmail.com>
2025-11-27 13:40:13 +0700

    Implement HomeViewModel using StateFlow for real-time data

---
+++ app/src/main/java/com/example/rommdb/viewmodel/HomeViewModel.kt
@@ -0,0 +1,29 @@
+package com.example.rommdb.viewmodel
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.rommdb.repositori.RepositoriSiswa
+import com.example.rommdb.room.Siswa
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.filterNotNull
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.stateIn
+
+class HomeViewModel(private val repositoriSiswa: RepositoriSiswa): ViewModel() {
+
+    companion object {
+        private const val TIMEOUT_MILLIS = 5_000L
+    }
+
+    val homeUiState: StateFlow<HomeUiState> = repositoriSiswa.getAllSiswaStream()
+        .filterNotNull()
+        .map { HomeUiState(ListSiswa = it.toList()) }
+        .stateIn(scope = viewModelScope,
+            started = SharingStarted.WhileSubscribed(stopTimeoutMillis = TIMEOUT_MILLIS),
+            initialValue = HomeUiState())
+}
+
+data class HomeUiState(
+    val ListSiswa: List<Siswa> = listOf()
+)
