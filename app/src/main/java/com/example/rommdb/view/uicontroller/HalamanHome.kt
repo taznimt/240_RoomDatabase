7a2e1f76358cedaceec3abf6af7592b27623f065
taznimt <taznimanwar06@gmail.com>
2025-11-27 12:07:02 +0700

    Add BodyHome Composable with empty state handling

--- app/src/main/java/com/example/rommdb/view/uicontroller/HalamanHome.kt
+++ app/src/main/java/com/example/rommdb/view/uicontroller/HalamanHome.kt
@@ -56,3 +56,22 @@ fun HomeScreen(
         )
     }
 }
+
+@Composable
+fun BodyHome(
+    itemSiswa: List<Siswa>,
+    modifier: Modifier = Modifier
+) {
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = modifier
+    ) {
+        if (itemSiswa.isEmpty()) {
+            Text(
+                text = stringResource("Tidak ada data Siswa. Tap + untuk menambah data"),
+                textAlign = TextAlign.Center,
+                style = MaterialTheme.typography.titleLarge
+            )
+        }
+    }
+}
