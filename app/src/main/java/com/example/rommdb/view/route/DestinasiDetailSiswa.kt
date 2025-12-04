cd0d1a2edacaccbf44b5f3402143a2f514905d7c
taznimt <taznimanwar06@gmail.com>
2025-12-04 09:03:30 +0700

    Add detail siswa navigation with ID argument

---
+++ app/src/main/java/com/example/rommdb/view/route/DestinasiDetailSiswa.kt
@@ -0,0 +1,10 @@
+package com.example.rommdb.view.route
+
+import com.example.rommdb.R
+
+object DestinasiDetailSiswa : DestinasiNavigasi {
+    override val route = "detail_siswa"
+    override val titleRes = R.string.detail_siswa
+    const val itemIdArg = "idSiswa"
+    val routeWithArgs = "$route/{$itemIdArg}"
+}
