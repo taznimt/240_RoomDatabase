5d960e77640bfe2deeb032459af9fe69fe669fa5
taznimt <taznimanwar06@gmail.com>
2025-12-04 09:21:52 +0700

    Add RepositoriSiswa interface and Offline implementation

--- app/src/main/java/com/example/rommdb/repositori/RepositoriSiswa.kt
+++ app/src/main/java/com/example/rommdb/repositori/RepositoriSiswa.kt
@@ -6,14 +6,18 @@ import kotlinx.coroutines.flow.Flow
 
 interface RepositoriSiswa {
     fun getAllSiswaStream(): Flow<List<Siswa>>
-
-    suspend fun insertSiswa(siswa: Siswa)  // Fixed the syntax error here
+    suspend fun insertSiswa(siswa: Siswa)
+    fun getSiswaStream(id: Int): Flow<Siswa>
+    suspend fun deteleSiswa(siswa: Siswa)
+    suspend fun updateSiswa(siswa: Siswa)
 }
 
 class OfflineRepositoriSiswa(
     private val siswaDao: SiswaDao
 ): RepositoriSiswa {
     override fun getAllSiswaStream(): Flow<List<Siswa>> = siswaDao.getAllSiswa()
-
     override suspend fun insertSiswa(siswa: Siswa) = siswaDao.insert(siswa)
+    override fun getSiswaStream(id: Int): Flow<Siswa> = siswaDao.getSiswa(id)
+    override suspend fun deteleSiswa(siswa: Siswa) = siswaDao.delete(siswa)
+    override suspend fun updateSiswa(siswa: Siswa) = siswaDao.update(siswa)
 }
