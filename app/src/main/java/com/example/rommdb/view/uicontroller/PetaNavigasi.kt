f7e78dd36e33d41dc16686272d1f121b4e9f531e
taznimt <taznimanwar06@gmail.com>
2025-12-04 12:09:55 +0700

    feat: Register Edit Student destination and define screen actions

--- app/src/main/java/com/example/rommdb/view/uicontroller/PetaNavigasi.kt
+++ app/src/main/java/com/example/rommdb/view/uicontroller/PetaNavigasi.kt
@@ -1,18 +1,22 @@
 package com.example.rommdb.view.uicontroller
 
+import android.R.attr.type
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.navigation.NavHostController
+import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
 import com.example.rommdb.view.DetailSiswaScreen
 import com.example.rommdb.view.route.DestinasiHome
 import com.example.rommdb.view.route.DestinasiEntry
 import com.example.rommdb.view.EntrySiswaScreen
 import com.example.rommdb.view.HomeScreen
 import com.example.rommdb.view.route.DestinasiDetailSiswa
+import com.example.rommdb.view.route.DestinasiDetailSiswa.itemIdArg
 
 @Composable
 fun SiswaApp(navController: NavHostController = rememberNavController(), modifier: Modifier ) {
@@ -49,6 +53,15 @@ fun HostNavigasi(
             DetailSiswaScreen(
                 navigateToEditItem = {navController.navigate("${DestinasiEditSiswa.route}/${it}")},
                 navigateBack = { navController.navigateUp() }
+            )
+        }
+        composable(route = DestinasiEditSiswa.routeWithArgs,
+            arguments = listOf(navArgument(DestinasiEditSiswa.itemIdArg) {
+                type = NavType.IntType})
+        ) {
+            EditSiswaScreen(
+                navigateBack = {navController.popBackStack()},
+                onNavigateUp = { navController.navigateUp() }
             )
         }
     }
