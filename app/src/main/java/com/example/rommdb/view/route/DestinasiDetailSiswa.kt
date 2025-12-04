8b28e80a5ba517702d77deb62b19e27d0d3e6d5b
taznimt <taznimanwar06@gmail.com>
2025-12-04 12:28:43 +0700

    fix: Correct incorrect route value and titleRes reference for Edit Siswa destination

--- app/src/main/java/com/example/rommdb/view/route/DestinasiDetailSiswa.kt
+++ app/src/main/java/com/example/rommdb/view/route/DestinasiDetailSiswa.kt
@@ -3,8 +3,8 @@ package com.example.rommdb.view.route
 import com.example.rommdb.R
 
 object DestinasiDetailSiswa : DestinasiNavigasi {
-    override val route = "detail_siswa"
-    override val titleRes = R.string.detail_siswa
+    override val route = "item_edit"
+    override val titleRes = R.string.edit_siswa
     const val itemIdArg = "idSiswa"
     val routeWithArgs = "$route/{$itemIdArg}"
 }
